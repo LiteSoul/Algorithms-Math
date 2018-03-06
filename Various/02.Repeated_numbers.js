d4ec1a5c0599ab1cd7536b7a63788f0f8ace7884
Ariel Blasi <exorcismo@gmail.com>
2018-03-06 16:12:39 -0300

    -Done-

--- Various/02.Repeated_numbers.js
+++ Various/02.Repeated_numbers.js
@@ -4,7 +4,7 @@ const digits = document.getElementById('digits')
 const button = document.getElementById('button')
 button.addEventListener('click', fireItUp)
 
-//This function fires the search function
+//This function fires the search function with necessary data
 function fireItUp() {
 	scanNdetect(input.value, digits.value)
 }
@@ -14,29 +14,34 @@ function scanNdetect(input, digits) {
 	const array = input.split('')
 	//Crate an object to store the search results
 	let storedResults = {}
-
+	//Create an array to iterate the search as per the number of digits
 	let digitizer = []
 	for (let i = 0; i < digits; i++) {
 		digitizer[i] = i + 1
 	}
-	console.log(digitizer)
 	digitizer.forEach(digit => {
-		console.log(digit)
+		//Create pivot as the chosen value to search
 		let mainPivot = ''
+		//Create iteration var to compare to chosen value
 		let mainIterated = ''
+		//Pivot will be iterated with iterator
 		let iterator = 0
+		//Iterated will be iterated with comparator
 		let comparator = iterator + 1
-		// function interaction(array,digits){
+		//Main iteration
 		for (iterator = 0; iterator < array.length; iterator++) {
 			mainPivot = array[iterator]
+			//If user requests more digits, pivot will grow in digits
 			if (digits > 1) {
 				for (let i = 1; i < digit; i++) {
 					mainPivot += array[iterator + i]
 				}
 			}
-			// mainPivot=array[iterator]+array[iterator+1]+array[iterator+2]
-			console.log('MAINPIVOT RECURSIVE ' + mainPivot)
+			//Create counter to count each repeated value
 			let counter = 1
+			//Stores the repeated value to the object as 'key', and
+			//how many times it's been repeated as 'value', if
+			//It hasn't been stored previously
 			if (storedResults[mainPivot] === undefined) {
 				for (
 					comparator = iterator + 1;
@@ -49,31 +54,27 @@ function scanNdetect(input, digits) {
 							mainIterated += array[comparator + c]
 						}
 					}
-					// let mainIterated = array[comparator]+array[comparator+1]+array[comparator+2]
-					console.log('MAINITERATED RECURSIVE ' + mainIterated)
 					if (mainPivot === mainIterated) {
 						counter++
-						console.log(counter)
 						storedResults[mainPivot] = counter
 					}
 				}
 			}
 		}
 	})
+	//Calls the printing function and sends the stored object
 	printResults(storedResults)
 }
 // This function prints the results stored in the object
 function printResults(object) {
 	let superString = ''
 	message.innerHTML = ''
-	console.log(object)
 	for (let key in object) {
-		// Check if the property really exists
+		// Check if the property really exists, avoids unindexed properties
 		if (object.hasOwnProperty(key)) {
 			let value = object[key]
 			superString = `The number <strong>${key}</strong> gets repeated <strong>${value} times</strong><br>`
 			message.innerHTML += superString
-			console.log(superString)
 		}
 	}
 }
